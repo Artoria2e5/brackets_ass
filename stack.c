4fc64a3ee64ffc29f3a26b0a7fee0d63b473be86
Mingye Wang <arthur200126@gmail.com>
2019-04-10 01:06:36 -0400

    stack: this should work

--- stack.c
+++ stack.c
@@ -5,36 +5,30 @@
 Stack* createStack(unsigned capacity){
 	Stack* stack = (Stack*) malloc(sizeof(Stack)); 
     stack->capacity = capacity; 
-    stack->top = -1; 
-    //TODO: Your choice here:
-	//stack->array = (Bracket*) malloc(stack->capacity * sizeof(Bracket));
-	//StackNode* head;	
+    stack->top = -1;
+    stack->array = (Bracket*) malloc(stack->capacity * sizeof(Bracket));
     return stack; 
 }
 
 // Stack is full when top index is equal to capacity-1
-int isFull(Stack* stack){
-	//TODO
-	return 0;
+int isFull(Stack* s){
+	return s->top == s->capacity - 1;
 }
 
 // Stack is empty when top is equal to -1 (or head of the list is NULL)
-int isEmpty(Stack* stack){
-	//TODO
-	return 0;
+int isEmpty(Stack* s){
+	return s->top == -1;
 }
 
 // Function to add an item to stack.  
-void push(Stack* stack, Bracket item){
-	if (isFull(stack)) 
-        return; 
-    //TODO
+void push(Stack* s, Bracket bk){
+	if (!isFull(s))
+		s->array[++(s->top)] = bk;
+	// as in the main program, let's not add error handling
 }
 
 // Function to remove an item from stack and return it. 
-Bracket pop(Stack* stack){
-	//TODO
-	Bracket top;
-	return top;
+Bracket pop(Stack* s){
+	return s->array[(s->top)--];
 }    
   
